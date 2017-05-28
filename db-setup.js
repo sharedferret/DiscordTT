efec96767bb8b56e97da775771508f084e519ed8
Kate Wellington <kate@katewellington.com>
2017-05-28 03:06:03 +0100

    Add sqlite db

--- db-setup.js
+++ db-setup.js
@@ -3,21 +3,46 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('db.sqlite3');
 
 db.serialize(function() {
+    console.log('- Creating Song');
   db.run('CREATE TABLE IF NOT EXISTS Song (' +
-    'id INTEGER PRIMARY KEY AUTOINCREMENT,' +
-    'type TEXT,' +
-    'url TEXT,' +
+    'id TEXT PRIMARY KEY NOT NULL,' +
+    'type TEXT NOT NULL,' +
+    'url TEXT NOT NULL,' +
     'artist TEXT,' +
-    'title TEXT)');
+    'title TEXT NOT NULL,' +
+    'metadata TEXT)');
 
-  db.run('CREATE TABLE IF NOT EXISTS Server (id TEXT PRIMARY KEY)');
+  console.log('- Creating Server');
+  db.run('CREATE TABLE IF NOT EXISTS Server (' +
+    'id TEXT PRIMARY KEY)');
 
-  db.run('CREATE TABLE IF NOT EXISTS User (id TEXT PRIMARY KEY,' +
-    'serverId TEXT, upvotes INTEGER, downvotes INTEGER, points INTEGER, lastActive DATETIME)');
+  console.log('- Creating User');
+  db.run('CREATE TABLE IF NOT EXISTS User (' +
+    'id TEXT PRIMARY KEY NOT NULL,' +
+    'serverId TEXT NOT NULL, ' +
+    'username TEXT, ' +
+    'avatar TEXT, ' +
+    'upvotes INTEGER DEFAULT 0 NOT NULL, ' +
+    'downvotes INTEGER DEFAULT 0 NOT NULL, ' +
+    'points INTEGER DEFAULT 0 NOT NULL, ' +
+    'lastActive DATETIME)');
 
-  db.run('CREATE TABLE IF NOT EXISTS UserQueue (userId TEXT PRIMARY KEY, position INTEGER, songId INTEGER)');
+  console.log('- Creating UserQueue');
+  db.run('CREATE TABLE IF NOT EXISTS UserQueue (' +
+    'id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+    'userId TEXT, ' +
+    'position INTEGER NOT NULL, ' +
+    'songId TEXT NOT NULL,' +
+    'dateAdded DATETIME)');
 
-  db.run('CREATE TABLE IF NOT EXISTS SongHistory (serverId TEXT, djid TEXT, songId INTEGER, time DATETIME, upvotes INTEGER, downvotes INTEGER)');
+  console.log('- Creating SongHistory');
+  db.run('CREATE TABLE IF NOT EXISTS SongHistory (' +
+    'serverId TEXT PRIMARY KEY NOT NULL, ' +
+    'djid TEXT NOT NULL, ' +
+    'songId TEXT NOT NULL, ' +
+    'time DATETIME, ' +
+    'upvotes INTEGER DEFAULT 0, ' +
+    'downvotes INTEGER DEFAULT 0)');
 });
 
 db.close();

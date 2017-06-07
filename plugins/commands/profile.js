6cf61b6589adbeb9cb80eb11ee70b6fb8cc4ea30
Kate Wellington <kate@katewellington.com>
2017-06-07 21:15:25 +0100

    Add most played songs to user profile, allow lookup of others' profiles

--- plugins/commands/profile.js
+++ plugins/commands/profile.js
@@ -6,14 +6,24 @@ var Discord = require('discord.js');
 var db = require(global.paths.lib + 'database-handler').db;
 
 var handleMessage = function(bot, message) {
+  if (message.mentions.users.size > 0) {
+    console.log('showing profile for first mention');
+    displayProfileForUser(bot, message, message.mentions.users.first());
+  } else {
+    displayProfileForUser(bot, message, message.author);
+  }
+};
+
+var displayProfileForUser = function(bot, message, user) {
   var embed = new Discord.RichEmbed();
 
-  embed.setAuthor(message.author.username, message.author.avatarURL);
+  embed.setAuthor(user.username, user.avatarURL);
   embed.setTimestamp(new Date());
-  embed.setThumbnail(message.author.avatarURL);
+  embed.setThumbnail(user.avatarURL);
+  embed.setFooter('Requested by ' + message.author.username, message.author.avatarURL);
 
   db.serialize(function() {
-    db.get('SELECT points FROM User WHERE id=?', [ message.author.id ], function(err, row) {
+    db.get('SELECT points FROM User WHERE id=?', [ user.id ], function(err, row) {
       var pointsText = '0 points';
 
       if (row && row.points) {
@@ -22,13 +32,28 @@ var handleMessage = function(bot, message) {
 
       embed.addField('DJ Points', pointsText, true);
 
-      message.channel.send('', { embed: embed });
+      db.all('SELECT s.title AS title, COUNT(*) AS playcount, SUM(h.upvotes) AS up, SUM(h.downvotes) AS down FROM SongHistory h LEFT JOIN Song s ON s.id = h.songId WHERE h.djid = ? GROUP BY songId ORDER BY playcount DESC LIMIT 3',
+        [ user.id ], function(mostPlayedErr, mostPlayedRows) {
+
+        var mostPlayed = '';
+
+        for (var i in mostPlayedRows) {
+          var row = mostPlayedRows[i];
+          mostPlayed += (parseInt(i) + 1) + ') ' + row.title + '\n\t\t' + row.playcount + ' plays (+' + row.up + '/-' + row.down + ')\n';
+        }
+
+        if (mostPlayed) {
+          embed.addField('Most Played', mostPlayed);
+        }
+
+        message.channel.send('', { embed: embed });
+      });
     });
   });
 };
 
 var matches = function(input) {
-  return name.indexOf(input.trim()) !== -1;
+  return _.startsWith(input, '/profile');
 };
 
 module.exports = {

989cd633d2690f2be417a20a0fdc1ee42320e86e
Kate Wellington <sharedferret@gmail.com>
2017-06-27 19:10:21 +0800

    Add playlist support

---
+++ plugins/commands/playlists.js
@@ -0,0 +1,24 @@
+const info = {
+  name: ['playlists', 'pl'],
+  description: 'View your playlists.',
+  usage: '`' + config.discriminator + 'playlists`',
+  type: CommandType.TTPlaylist
+};
+
+const playlistHandler = require(global.paths.lib + 'playlist-handler');
+const tt = require(global.paths.lib + 'turntable-handler');
+const Discord = require('discord.js');
+
+const handleMessage = function(bot, message) {
+  playlistHandler.viewPlaylists(bot, message);
+};
+
+const matches = function(input) {
+  return info.name.map(function(i) { return config.discriminator + i; }).indexOf(input.trim()) !== -1;
+};
+
+module.exports = {
+  info: info,
+  handleMessage: handleMessage,
+  matches: matches
+};

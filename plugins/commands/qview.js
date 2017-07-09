676e6164ea13f8ba4aa5b4e09858d8ad1ac1d093
Kate Wellington <kate@katewellington.com>
2017-07-09 12:06:26 +0100

    Queue pagination

--- plugins/commands/qview.js
+++ plugins/commands/qview.js
@@ -1,7 +1,7 @@
 const info = {
   name: ['q'],
-  description: 'View your playlist.',
-  usage: '`' + config.discriminator + 'q`\n`' + config.discriminator + 'q`',
+  description: 'View your current playlist.',
+  usage: '`' + config.discriminator + 'q`: View the first page of your queue.\n`' + config.discriminator + 'q [page number]`: View a specific page of your active playlist\'s queue.',
   type: CommandType.TTPlaylist
 };
 
@@ -10,11 +10,32 @@ const tt = require(global.paths.lib + 'turntable-handler');
 const Discord = require('discord.js');
 
 const handleMessage = function(bot, message) {
-  queueHandler.viewQueue(bot, message);
+  const cmd = message.content.substring(config.discriminator.length, message.content.length);
+
+  let page = 1;
+
+  if (cmd.length > 2) {
+    const idx = parseInt(cmd.substring(2, cmd.length), 10);
+    console.log('idx', idx);
+
+    if (!isNaN(idx)) {
+      page = idx;
+    }
+  }
+
+  queueHandler.viewQueue(bot, message, page);
 };
 
 const matches = function(input) {
-  return info.name.map(function(i) { return config.discriminator + i; }).indexOf(input.trim()) !== -1;
+  // Match /q
+  if (info.name.map(function(i) { return config.discriminator + i; }).indexOf(input.trim()) !== -1) return true;
+
+  // Match /q [page num]
+  if (_.startsWith(input, config.discriminator + 'q ')) {
+    const idx = parseInt(input.substring(config.discriminator.length + 2, input.length), 10);
+    return !isNaN(idx);
+  }
+
 };
 
 module.exports = {

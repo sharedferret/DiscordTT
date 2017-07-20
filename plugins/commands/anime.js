d7ae64d135845a9633d1d3c07e9a7a5755f1bb3a
Kate Wellington <sharedferret@gmail.com>
2017-07-20 19:16:34 +0100

    Add streaming link to /anime response

--- plugins/commands/anime.js
+++ plugins/commands/anime.js
@@ -21,30 +21,48 @@ const handleMessage = function(bot, message) {
 
       const result = parsedBody.data[0];
 
-      const embed = new Discord.RichEmbed();
+      // Grab streaming links
+      request('https://kitsu.io/api/edge/anime/' + result.id + '/streaming-links',
+      function(slErr, slResponse, slBody) {
+        const parsedSlBody = JSON.parse(slBody);
+        const streamingLinkData = parsedSlBody.data;
 
-      embed.setTitle(result.attributes.canonicalTitle);
-      embed.setDescription(result.attributes.synopsis);
+        const streamingLinks = [];
 
-      embed.addField('Type', result.attributes.subtype, true);
-      embed.addField('Episodes', result.attributes.episodeCount, true);
-      embed.addField('Status', result.attributes.status, true);
-      embed.addField('Rating', result.attributes.averageRating, true);
-      embed.addField('Popularity', result.attributes.popularityRank, true);
-      embed.addField('Aired', result.attributes.startDate + ' to ' + result.attributes.endDate, true);
-      embed.addField('Link', 'https://kitsu.io/anime/' + result.attributes.slug);
+        for (const streamingLink of streamingLinkData) {
+          streamingLinks.push(streamingLink.attributes.url);
+        }
 
-      const altTitles = [];
-      if (result.attributes.titles.en) altTitles.push('_Romanized_: ' + result.attributes.titles.en);
-      if (result.attributes.titles.ja_jp) altTitles.push('_Japanese_: ' + result.attributes.titles.ja_jp); 
-      embed.addField('Alternate Titles', altTitles.join(', '));
+        const embed = new Discord.RichEmbed();
 
-      embed.setFooter('Requested by ' + message.author.username, message.author.avatarURL);
-      embed.setTimestamp(new Date());
-      embed.setThumbnail(result.attributes.posterImage.small);
+        embed.setTitle(result.attributes.canonicalTitle);
+        embed.setDescription(result.attributes.synopsis);
 
-      message.channel.send('', { embed: embed });
-      console.log('res', result);
+        embed.addField('Type', result.attributes.subtype, true);
+        embed.addField('Episodes', result.attributes.episodeCount, true);
+        embed.addField('Status', result.attributes.status, true);
+        embed.addField('Rating', result.attributes.averageRating, true);
+        embed.addField('Popularity', result.attributes.popularityRank, true);
+        embed.addField('Aired', result.attributes.startDate + ' to ' + result.attributes.endDate, true);
+        embed.addField('Link', 'https://kitsu.io/anime/' + result.attributes.slug);
+
+        if (streamingLinks.length > 0) {
+          embed.addField('Streaming Links', streamingLinks.join('\n'));
+        }
+        
+        const altTitles = [];
+        if (result.attributes.titles.en) altTitles.push('_Romanized_: ' + result.attributes.titles.en);
+        if (result.attributes.titles.ja_jp) altTitles.push('_Japanese_: ' + result.attributes.titles.ja_jp); 
+        embed.addField('Alternate Titles', altTitles.join('\n'));
+
+        // TODO: Cache this data
+
+        embed.setFooter('Requested by ' + message.author.username, message.author.avatarURL);
+        embed.setTimestamp(new Date());
+        embed.setThumbnail(result.attributes.posterImage.small);
+
+        message.channel.send('', { embed: embed });
+      });
     } catch (e) {
       console.warn('anime lookup failure', e);
     }
